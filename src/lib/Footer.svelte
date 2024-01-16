f49d1aa1ca532171da8611344c16059e3f2514b9
silloi <silloi1993@gmail.com>
2024-01-17 00:07:11 +0900

    implement memo app

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let color = "";
+	export let updateColor = (color: string) => {};
+	export let next = () => {};
+	export let first = () => {};
+
+	let colorPicker: HTMLInputElement;
+
+	const openColorPicker = () => {
+		colorPicker.focus();
+		colorPicker.value = color;
+    colorPicker.click();
+	};
+
+	const handleChangeColor = (e: any) => {
+		updateColor(e.target.value);
+	};
+</script>
+
+<footer class="flex justify-between">
+	<input type="color" bind:value={color} bind:this={colorPicker} on:input={handleChangeColor} hidden />
+	<button class="btn btn-circle btn-outline picker" style="--color:{color}" on:click={openColorPicker}></button>
+	<div>
+		<button on:click={first} class="btn btn-circle btn-outline">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 16 16" stroke="currentColor">
+        <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+        <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+			</svg>
+		</button>
+		<button on:click={next} class="btn btn-circle btn-outline">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 16 16" stroke="currentColor"><path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/></svg>
+		</button>
+	</div>
+</footer>
+
+<style>
+	.picker {
+		background-color: var(--color);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { browser } from "$app/environment";
+	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
+
+	// const COLORS = ["#ABDEE6", "#CBAACB", "#FFFFB5", "#FFCCB6", "#F3B0C3"];
+	const COLORS = ["#00A5E3", "#8DD7BF", "#FF96C5", "#FF5768", "#FFBF65"];
+	
+	const pickColor = (index: number) => COLORS[index % COLORS.length];
+
+	const initializeTodoList = () => {
+		const todoListString = browser ? localStorage.getItem("todos") : "";
+		if (todoListString) {
+			return JSON.parse(todoListString);
+		}
+		const initialTodo = [{ content: "", color: pickColor(0) }];
+		return initialTodo;
+	};
+
+	let todoList = initializeTodoList();
+	let currentIndex = 0;
+
+	let textareaElement: HTMLTextAreaElement;
+
+  const updateLocalStorage = (todoList: any) => {
+    browser && localStorage.setItem("todos", JSON.stringify(todoList));
+  };
+
+  const updateContent = (value: string, index: number) => {
+    const newTodo = { ...todoList[index], content: value };
+    todoList[index] = newTodo;
+    updateLocalStorage(todoList);
+  };
+
+	const createTodo = () => {
+		const newTodo = {
+			content: "",
+			color: pickColor(todoList.length),
+		};
+		todoList = [newTodo, ...todoList];
+    updateLocalStorage(todoList);
+		textareaElement.focus();
+	};
+
+	const archiveTodo = () => {
+		if (todoList.length <= 1) {
+			return;
+		}
+		const isLatest = currentIndex === 0;
+		todoList = todoList.filter((_: any, index: number) => index !== currentIndex);
+		updateLocalStorage(todoList);
+		if (!isLatest) {
+			currentIndex--;
+		}
+	};
+
+	const updateColor = (newColor: string) => {
+		todoList[currentIndex].color = newColor;
+		updateLocalStorage(todoList);
+	};
+
+	const goNextTodo = () => {
+		currentIndex = (currentIndex + 1) % todoList.length;
+	};
+
+	const goFirstTodo = () => {
+		currentIndex = 0;
+	};
+</script>
+
+<main class="flex flex-col h-screen w-full p-4">
+	<Header create={createTodo} archive={archiveTodo} current={currentIndex + 1} length={todoList.length}  />
+	{#each todoList as todo, todoIndex}
+		{#if todoIndex === currentIndex}
+			<article class="flex-1 px-12"> 
+				<textarea bind:value={todo.content} bind:this={textareaElement} on:input={(e) => updateContent(e.currentTarget.value, todoIndex)} style="--color: {todo.color}" placeholder="Todo..." class="textarea h-full w-full text-8xl text-black"></textarea>
+			</article>
+		{/if}
+	{/each}
+	<Footer color={todoList[currentIndex].color} updateColor={updateColor} next={goNextTodo} first={goFirstTodo} />
+</main>
+
+<style>
+	textarea {
+		background-color: var(--color);
+	}
+</style>
