3cafefa998ba6901c0b80977c43be4f390f944dc
silloi <silloi1993@gmail.com>
2024-01-20 15:05:55 +0900

    use vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			includeAssets: ['favicon.png', 'apple-touch-icon.png', 'memo.svg'],
+			manifest: {
+				name: 'Stickstack',
+				short_name: 'Stickstack',
+				description: 'Sticky-like stackable todo app',
+				icons: [
+					{
+						src: 'android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 });
